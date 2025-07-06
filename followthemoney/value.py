e61daf541a0b8c76c79776c0b652c3d939e3529a
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-07-06 14:13:53 +0200

    Type checker be cracking up

--- followthemoney/value.py
+++ followthemoney/value.py
@@ -6,9 +6,9 @@ from prefixdate import DatePrefix
 from followthemoney.util import sanitize_text
 
 if typing.TYPE_CHECKING:
-    from followthemoney.proxy import E
+    from followthemoney.proxy import EntityProxy
 
-Value = Union[str, int, float, bool, date, datetime, DatePrefix, None, "E"]
+Value = Union[str, int, float, bool, date, datetime, DatePrefix, None, "EntityProxy"]
 Values = Union[Value, Sequence[Value], Set[Value]]
 
 
@@ -45,14 +45,14 @@ def string_list(value: Any, sanitize: bool = False) -> List[str]:
         return texts
     if isinstance(value, DatePrefix):
         return [value.text] if value.text else []
-    if isinstance(value, Mapping):
-        text = value.get("id")
-        return [text] if text is not None else []
     # EntityProxy
     try:
         return string_list(value.id, sanitize=sanitize)
     except AttributeError:
         pass
+    # Entity dict
+    if isinstance(value, Mapping):
+        return string_list(value.get("id"), sanitize=sanitize)
     if isinstance(value, Sequence):
         stexts: List[str] = []
         for inner in value:
